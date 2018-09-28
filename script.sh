423f3883be5797572bd9b443e025d8a37e5b85e2
black-apple <498460828@qq.com>
2018-09-28 22:28:52 +0800

    添加脚本

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash 
+
+# 移动文件
+rm -rf docs
+mkdir docs
+cp -a html/Category/* docs/
+
+cp favicon.ico /docs/
+
+# # 获取当前分支名
+branch=`git symbolic-ref --short -q HEAD`
+# push 注意是ref不是refs
+git push origin ${branch}
